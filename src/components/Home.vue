8f6dd431df41003336b7236c6ad59362567ed33a
Sarkunan <sarkunan92@gmail.com>
2019-04-10 22:22:50 +0800

    1. Updated links in the home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,47 +6,63 @@
           Hmm
         </p>
       </v-card>
-    </v-flex> -->
-     <v-flex d-flex xs12>
-        <v-card flat style="" class="text-container center-align-all">
-          <p>Waiting for something amazing to put here.</p>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs12>
-        <v-card flat class="text-container center-align-all">
-          <!-- <div>
+    </v-flex>-->
+    <v-flex d-flex xs12>
+      <v-card flat style class="text-container center-align-all">
+        <p>Waiting for something amazing to put here.</p>
+      </v-card>
+    </v-flex>
+    <v-flex d-flex xs12>
+      <v-card flat class="text-container center-align-all">
+        <!-- <div>
             <div class="circle">
 
             </div>
-          </div> -->
-          <div style="text-align: left;">
-            <p style="font-size: 30px;">
-              <span style="display: block; margin-bottom: -10px;">Sarkunan V.</span>
-              <span style="font-size: 18px;">Software Engineer</span>
-            </p>
-            <p>
-              Hello, <br />
-              I'm currently building out a simple portfolio page using VueJS. <br />
-              Tweet at me
-              <a href="https://twitter.com/raxIsBlur">@raxIsBlur</a>
-              or you can find me on
-              <a href="https://github.com/raxIsBlur">Github</a>
-            </p>
-            <p>
-              <v-icon size="36px">{{ "fa fa-github" }}</v-icon>
-            </p>
+        </div>-->
+        <div style="text-align: left;">
+          <p style="font-size: 30px;">
+            <span style="display: block; margin-bottom: -10px;">Sarkunan V.</span>
+            <span style="font-size: 18px;">Software Engineer</span>
+          </p>
+          <p>
+            Hello,
+            <br>
+            I'm currently building out a simple portfolio page using VueJS.
+          </p>
+          <p>Find me on one of these if you've visited :)</p>
+          <div>
+            <v-btn v-for="icon in icons" :key="icon.name" :href="icon.link" class="" small dark style="height: 15px!important;">
+              <v-icon size="36px">{{ icon.name }}</v-icon>
+            </v-btn>
           </div>
-        </v-card>
-      </v-flex>
+        </div>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { SOCIAL_LINKS } from './common/Constants.js'
+
 export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Some random message'
+      msg: 'Some random message',
+      icons: [
+        {
+          name: SOCIAL_LINKS.github.icon,
+          link: SOCIAL_LINKS.github.url
+        },
+        {
+          name: SOCIAL_LINKS.twitter.icon,
+          link: SOCIAL_LINKS.twitter.url
+        },
+        {
+          name: SOCIAL_LINKS.linkedin.icon,
+          link: SOCIAL_LINKS.linkedin.url
+        }
+      ]
     }
   }
 }
@@ -64,7 +80,6 @@ export default {
   /* box-shadow: none; */
   text-align: center;
   display: flex;
-
 }
 
 .center-align-all {
