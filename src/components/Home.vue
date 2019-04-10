027c311abfc42043d0a326876a9e9aeb00eb0690
Sarkunan <sarkunan92@gmail.com>
2019-04-10 21:46:42 +0800

    1. Updated some content for the main site

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,41 @@
 <template>
   <v-layout row wrap>
-    <v-flex d-flex xs12>
+    <!-- <v-flex d-flex xs12>
       <v-card flat class="text-container center-align-all">
         <p>
-          Hello
+          Hmm
         </p>
       </v-card>
-    </v-flex>
+    </v-flex> -->
      <v-flex d-flex xs12>
-        <v-card flat class="text-container center-align-all">
-          <p>Hellooooo...</p>
+        <v-card flat style="" class="text-container center-align-all">
+          <p>Waiting for something amazing to put here.</p>
         </v-card>
       </v-flex>
       <v-flex d-flex xs12>
         <v-card flat class="text-container center-align-all">
-          <p>
-            Hello, <br />
-            I'm currently building out a simple portfolio page using VueJS. <br />
-            Tweet at me
-            <a href="https://twitter.com/raxIsBlur">@raxIsBlur</a>
-            or you can find me on
-            <a href="https://github.com/raxIsBlur">Github</a>
-          </p>
+          <!-- <div>
+            <div class="circle">
+
+            </div>
+          </div> -->
+          <div style="text-align: left;">
+            <p style="font-size: 30px;">
+              <span style="display: block; margin-bottom: -10px;">Sarkunan V.</span>
+              <span style="font-size: 18px;">Software Engineer</span>
+            </p>
+            <p>
+              Hello, <br />
+              I'm currently building out a simple portfolio page using VueJS. <br />
+              Tweet at me
+              <a href="https://twitter.com/raxIsBlur">@raxIsBlur</a>
+              or you can find me on
+              <a href="https://github.com/raxIsBlur">Github</a>
+            </p>
+            <p>
+              <v-icon size="36px">{{ "fa fa-github" }}</v-icon>
+            </p>
+          </div>
         </v-card>
       </v-flex>
   </v-layout>
@@ -56,5 +70,14 @@ export default {
 .center-align-all {
   align-items: center;
   justify-content: center;
+}
+
+.circle {
+  width: 100px;
+  height: 100px;
+  background: blue;
+  -moz-border-radius: 50px;
+  -webkit-border-radius: 50px;
+  border-radius: 50%;
 }
 </style>
